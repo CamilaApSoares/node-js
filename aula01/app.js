21ec6fd7c2f197a7172392fe4667ba5d7d741665
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2024-02-23 10:26:59 -0300

    Primeira aula NodeJS

---
+++ aula01/app.js
@@ -0,0 +1,11 @@
+var adicao = require ('./adicao')
+console.log(adicao(10,2))
+
+var subt = require ('./subt')
+console.log(subt(10,2))
+
+var mult = require ('./mult')
+console.log(mult(10,2))
+
+var div = require ('./div')
+console.log(div(10,2))
---
+++ aula01/subt.js
@@ -0,0 +1,6 @@
+var subt = function(n1,n2){
+    return n1-n2
+}
+
+module.exports = subt
+
